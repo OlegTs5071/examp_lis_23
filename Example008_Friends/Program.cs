90ba6f05379a0adf3c2776f66d8618d4bde3255f
Oleg <cukoleg@yandex.ru>
2023-09-05 23:13:47 +0300

    Сохранили изменения в задачах

---
+++ Example008_Friends/Program.cs
@@ -0,0 +1,18 @@
+﻿int vf1 = 1, vf2 = 2,
+    vd = 5, f = 2,
+    s = 10000, count = 0;
+while (s > 10)
+{   
+    int tm = 0;
+    if (f = f - 1)
+    {
+        tm = tm + s / (vf1 + vd);
+        f = 2;
+    }
+    tm = tm + s / (vf2 + vd);
+    f = f - 1;
+    s = s - (vf1 + vf2)*tm;
+   Console.Write(count); 
+   count++;
+    
+}
