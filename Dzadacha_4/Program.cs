be74b11d9105396a970d6dea1faebc40ac76461d
Oleg <cukoleg@yandex.ru>
2023-09-10 15:46:07 +0300

    added new programs

---
+++ Dzadacha_4/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и 
+выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
+}
+int a = 1121;
+int b = 52161;
+int c = 778739;
+
+int max = Max(a, b, c);
+Console.WriteLine(max);
