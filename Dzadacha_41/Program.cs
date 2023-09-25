a85370033d1ca64e0080363a0d9d1aa1ea741bb8
Oleg <cukoleg@yandex.ru>
2023-09-25 18:36:48 +0300

    added new completed new tasks 38 41 43

---
+++ Dzadacha_41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] GetArrayNumbers(int M)
+{
+    int[] array = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        Console.WriteLine("Введите число");
+        array[i] = Convert.ToInt32(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int CountNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;            
+        }
+    }
+    return count;              
+}
+
+int M = 6;
+int[] massive = GetArrayNumbers(M);
+int number = CountNumber(massive);
+Console.WriteLine(number);
+
---
+++ Dzadacha_43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, 
+// b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double HorizontalLineX(double k1, double b1, double k2, double b2)
+{
+    double b = b2 - b1;
+    double k = k1 - k2;
+    double x = b/k;
+    return x;
+}
+
+double VerticalLineY(double x, double k1, double b1)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
+double b1 = 2;
+double k1 = 5;
+double b2 = 4;
+double k2 = 9;
+double x = HorizontalLineX(k1, b1, k2, b2);
+double y= VerticalLineY(x, k1, b1);
+Console.WriteLine($"x = {x:f}");
+Console.WriteLine($"y = {y:f}");
